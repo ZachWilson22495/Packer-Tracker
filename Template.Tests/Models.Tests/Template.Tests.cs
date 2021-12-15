2199ca3ee1d7c8f345ff345dd9ab8d52285f18fd
Zach Wilson <92273347+ZachWilson22495@users.noreply.github.com>
2021-12-15 10:01:05 -0800

    Initial commit

---
+++ Template.Tests/Models.Tests/Template.Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using Template.Models;
+
+namespace Template.Models.Tests
+{
+  [TestClass]
+  public class TemplateTests
+  {
+    [TestMethod]
+    public void ThingYoureTesting_WhatYouWantItToDo_OutcomeDataType()
+    {
---
+++ Template/Models/Template.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace Template.Models
+{
+  public class Template
+  {
+    string work = "work pls?"
+  }
+}
